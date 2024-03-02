ee9cda8a913401ea26fe9cb363b0834bdda9dede
Nathana2022 <nathanaeldon495@gmail.com>
2024-03-02 22:20:10 +0100

    Feat : Ajout de la fonction de calcul de la somme des âges et de tri des personnes avec un âge minimum

--- calculer_somme_age.js
+++ calculer_somme_age.js
@@ -9,6 +9,22 @@ const personnes = [
   ];
   const ageMinimum = 25;
 
+  function calculerSommeAge(personnes, ageMinimum){
+    const personnesFiltrees = personnes.filter(personnes => 
+      personnes.age>= ageMinimum);
+    const sommeAges = personnesFiltrees.reduce((total, personnes) => 
+      total + personnes.age, 0);
+    const personnesTrie = personnesFiltrees.sort((a, b) => a.nom.localeCompare(a.nom));
+
+          return {sommeAges , personnesTrie};
+
+
+  }
+
+  const {sommeAges, personnesTrie} = calculerSommeAge(personnes, ageMinimum);
+  console.log('Somme des âges des personnes de', ageMinimum, 'ans et plus :', sommeAges);
+  console.log('Personnes de', ageMinimum, 'ans et plus triées par ordre alphabétique :', personnesTrie);
+
 
   /**
    * Ex: Somme des âges : 75

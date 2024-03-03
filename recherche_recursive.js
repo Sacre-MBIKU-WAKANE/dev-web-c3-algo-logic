ffe5f0b225a251c1a16c5e6f9fe21a011e12a28f
Nathana2022 <nathanaeldon495@gmail.com>
2024-03-03 21:12:47 +0100

    Feat : Ajout de la fonction de recherche recursive

--- recherche_recursive.js
+++ recherche_recursive.js
@@ -17,6 +17,30 @@ const structure = {
 };
 const recherche = 'D';
 
+function chercherElement(structure, recherche, chemin = []) {
+  if (structure.nom === recherche) {
+      chemin.push(structure.nom);
+      return chemin;
+  }
+
+  for (let enfant of structure.enfants) {
+      const resultat = chercherElement(enfant, recherche, [...chemin, structure.nom]);
+      if (resultat) {
+          return resultat;
+      }
+  }
+
+  return null;
+}
+
+const cheminElement = chercherElement(structure, recherche);
+
+if (cheminElement) {
+  console.log(`Chemin vers l'élément "${recherche}" : ${cheminElement.join(' -> ')}`);
+} else {
+  console.log(`L'élément "${recherche}" n'a pas été trouvé dans la structure.`);
+}
+
 
 /**
  * Ex : [0, 1] (ce qui signifie que 'D' se trouve dans la première branche à l'indice 0, puis dans la deuxième branche à l'indice 1)

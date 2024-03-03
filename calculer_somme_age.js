1c9ca9737bd2aaa1ef28a5d816d31d926986e943
chadrackmbala <chadrackmbala20@gmail.com>
2024-03-03 20:58:36 +0100

    fit calculer_somme_age.js

--- calculer_somme_age.js
+++ calculer_somme_age.js
@@ -3,13 +3,37 @@
 // ________________________________
 
 const personnes = [
-    { nom: 'Alice', age: 25 },
-    { nom: 'Bob', age: 30 },
-    { nom: 'Charlie', age: 20 }
-  ];
-  const ageMinimum = 25;
+  { nom: 'Alice', age: 25 },
+  { nom: 'Bob', age: 30 },
+  { nom: 'Charlie', age: 20 }
+];
+const ageMinimum = 25;
 
+function calculeSommmeAge() {
+  let sommeAge = 0
+  personnes.forEach(age => {
+    sommeAge = sommeAge + age.age
+  });
+  return sommeAge;
+}
+console.log(calculeSommmeAge());
 
-  /**
-   * Ex: Somme des âges : 75
-   */
+function trieUtilisateur() {
+  
+  let arrayOfNames = [];
+
+  personnes.forEach((personne) => {
+    arrayOfNames.push(personne.nom);
+  })
+
+  const trie = arrayOfNames.sort();
+
+  
+  return trie;
+}
+
+console.log(trieUtilisateur());
+
+/**
+ * Ex: Somme des âges : 75
+ */
